6a5b9e687567e678ce795dab4e6777373ca60148
Ignacio Reyna <ignacioreyna56@gmail.com>
2019-11-08 15:28:33 -0600

    [ StarWars ] - Inicio del proyecto (Build)

---
+++ build/precache-manifest.ff8a231985ef6a5311e1704d356dee0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e058273502ad9d27e857c8a2fc5aa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98dcb1f81296ca2665e0",
+    "url": "/static/css/main.3bae8b30.chunk.css"
+  },
+  {
+    "revision": "1c12545faf31d86ce5a5",
+    "url": "/static/js/2.4f630884.chunk.js"
+  },
+  {
+    "revision": "98dcb1f81296ca2665e0",
+    "url": "/static/js/main.ca3f8a45.chunk.js"
+  },
+  {
+    "revision": "90d2e373c5552563a8bc",
+    "url": "/static/js/runtime-main.ed4b9687.js"
+  }
+]);
