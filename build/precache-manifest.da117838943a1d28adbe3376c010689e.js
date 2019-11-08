3938f0d8b7e0dddbd034076fb237dddd69a6fddf
Ignacio Reyna <ignacioreyna56@gmail.com>
2019-11-08 17:17:07 -0600

    make it better

---
+++ build/precache-manifest.da117838943a1d28adbe3376c010689e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7628e5a5bc400295cdb7a13ab6e7ab14",
+    "url": "/starwars.nachoreyna/index.html"
+  },
+  {
+    "revision": "2dd491c4f8b499387b83",
+    "url": "/starwars.nachoreyna/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "7016e9a4b4f831e31487",
+    "url": "/starwars.nachoreyna/static/css/main.e3418690.chunk.css"
+  },
+  {
+    "revision": "2dd491c4f8b499387b83",
+    "url": "/starwars.nachoreyna/static/js/2.d02e3980.chunk.js"
+  },
+  {
+    "revision": "7016e9a4b4f831e31487",
+    "url": "/starwars.nachoreyna/static/js/main.3b269e00.chunk.js"
+  },
+  {
+    "revision": "b230390cec509ef0d0f9",
+    "url": "/starwars.nachoreyna/static/js/runtime-main.7dee12a1.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3bd0ba37ea4e1bf249d686bc20910f5.js"
+  "/starwars.nachoreyna/precache-manifest.da117838943a1d28adbe3376c010689e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starwars.nachoreyna/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
